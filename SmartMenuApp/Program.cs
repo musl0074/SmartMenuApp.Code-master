4e4ba363d185e6430ec13681ad64169009d032fd
Marcus Vikkelsøe Søndergaard Nielsen <marc6640@edu.eal.dk>
2018-09-26 11:55:13 +0200

    Program ændret

--- SmartMenuApp/Program.cs
+++ SmartMenuApp/Program.cs
@@ -19,7 +19,7 @@ namespace SmartMenuApp
         private void Run()
         {
             SmartMenu menu = new SmartMenu();
-            menu.LoadMenu(@"C:\Users\Muslim Al Ali\Desktop\Datamatiker - 2018\Programmering\Projekt\SmartMenuApp.Code-master\MenuSpec.txt");
+            menu.LoadMenu(@"C:\Users\Marcu\Documents\SmartMenuApp.Code-master\MenuSpec.txt.Code-master\MenuSpec.txt");
             menu.Activate();
         }
     }
