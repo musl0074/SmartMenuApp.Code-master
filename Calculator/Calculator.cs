0708a4cef5a6a7e0c03426e777f846d7826b3047
Rasmus Kjær Dam <rasm179f@edu.eal.dk>
2018-10-03 12:20:43 +0200

    Added CalculatorLibrary, TestVersion1

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public static class Calculator
+    {
+        public static double Add(double a, double b) {
+            return a + b;
+        }
+        public static double Subtract(double a, double b) {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b) {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b) {
+            return a / b;
+        }
+
+        public static double Sum(double[] numbers) {
+            double sum = 0;
+            foreach (double number in numbers) {
+                sum += number;
+            }
+            return sum;
+        }
+
+        public static double Minimum(double[] numbers) {
+            double small = numbers[0];
+            foreach (double number in numbers) {
+                if (small > number) {
+                    small = number;
+                }
+            }
+            return small;
+        }
+
+        public static double Maximum(double[] numbers) {
+            double big = numbers[0];
+            foreach (double number in numbers) {
+                if (big < number) {
+                    big = number;
+                }
+            }
+            return big;
+        }
+
+        public static double Average(double[] numbers) {
+            return Sum(numbers) / numbers.Length;
+        }
+    }
+}
---
+++ TestVersion1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestVersion1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestVersion1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c262f16c-0859-47da-b18d-bcb547ba22dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
