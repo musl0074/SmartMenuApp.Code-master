8f37c7322e3999d9761394cc335ba9b7010c021e
Muslim Al-Ali <musl0074@edu.eal.dk>
2018-10-04 11:39:53 +0200

    Creating SmartMenuLibrary and its tests

---
+++ SmartMenuLibrary2/SmartMenu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenuLibrary2
+{
+    public class SmartMenu
+    {
+        string titel, menuValg;
+        string[] menuPunkt, menuID;
+
+        public void LoadMenu(string path)
+        {
+
+            string line;
+            List<string> lines = new List<string>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            // Fyld vores liste med tekstfilen linje for linje
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            // Sæt de to unikke variabler
+            titel = lines[0];
+            menuValg = lines[1];
+
+            menuPunkt = new string[lines.Count - 2];
+            menuID = new string[lines.Count - 2];
+
+            // Sæt resten af variablerne
+            for (int i = 2; i < lines.Count; i++)
+            {
+                string[] split = lines[i].Split(';');
+                menuPunkt[i - 2] = split[0];
+                menuID[i - 2] = split[1];
+            }
+
+            file.Close();
+        }
+        public void Activate(IBindings bindings)
+        {
+            int brugervalg = 0;
+
+            Console.WriteLine(titel);
+            foreach (string punkt in menuPunkt)
+            {
+                Console.WriteLine(punkt);
+            }
+            Console.WriteLine(menuValg);
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+                // Exit hvis input er "0" eller en tom linje0
+                if (input == "0" || input == "")
+                {
+                    break;
+                }
+                else if (int.TryParse(input, out brugervalg))
+                {
+                    brugervalg--;
+                    if (brugervalg < menuID.Length && brugervalg > 0)
+                    {
+                        bindings.Call(menuID[brugervalg]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ugyldigt Input");
+                }
+            }
+        }
+    }
+}
+
+    
+
---
+++ SmartMenuTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SmartMenuTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmartMenuTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3da22c1-19f6-4d3a-a604-3301ad78d2aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
